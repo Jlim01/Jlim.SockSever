05cae58095fabb94dab6add085699f22853bc6f9
Forest_Jun <76178551+Jlim01@users.noreply.github.com>
2025-05-24 21:09:31 +0900

    MahApps.Metro SET UP

--- Jlim.SockSever/Views/MainPage.xaml.cs
+++ Jlim.SockSever/Views/MainPage.xaml.cs
@@ -1,24 +1,12 @@
-﻿using RestrauntHost.Main.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using MahApps.Metro.Controls;
+using RestrauntHost.Main.Interfaces;
 
 namespace RestrauntHost.Main.Views
 {
     /// <summary>
     /// Interaction logic for MainPage.xaml
     /// </summary>
-    public partial class MainPage : Window
+    public partial class MainPage : MetroWindow
     {
         public MainPage(IMainViewModel viewModel)
         {
